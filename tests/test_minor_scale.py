310977d9ac46179c8be277c7fe927c311cdecbd8
Sergei Gerasenko <sgerasenko@conversantmedia.com>
2020-02-17 23:18:37 -0600

    wip:Mon_Feb_17_23:18:37_2020

--- tests/test_minor_scale.py
+++ tests/test_minor_scale.py
@@ -4,11 +4,23 @@
 import yaml
 from chord_finder.minor_scale import MinorScale
 from chord_finder.common import ENHARM_SHARP_MODE
+from chord_finder.common import ENHARM_FLAT_MODE
 
 
-def test_build_major_sharp_scales():
+def test_build_minor_sharp_scales():
     mode = ENHARM_SHARP_MODE
     with open('tests/data/minor_sharp_scales.yaml') as handle:
+        data = yaml.load(handle, Loader=yaml.FullLoader)
+
+    scales = data['scales']
+    for root in scales:
+        scale = MinorScale(root, mode)
+        assert scale.get_members() == scales[root]
+
+
+def test_build_minor_flat_scales():
+    mode = ENHARM_FLAT_MODE
+    with open('tests/data/minor_flat_scales.yaml') as handle:
         data = yaml.load(handle, Loader=yaml.FullLoader)
 
     scales = data['scales']
